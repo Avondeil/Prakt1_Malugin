72fb12c015338b895dd350d524ad44ee7c79e2ca
Avondeil <123551851+Avondeil@users.noreply.github.com>
2024-02-17 12:56:23 +0500

    Add files via upload

---
+++ Prakt1.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter: ";
+    int numb, sum = 0;
+    cin >> numb;
+    for (int i = 1; i < numb; i++)
+    {
+        if ((i % 2) != 0)
+        {
+            sum += i;
+        }
+    }
+
+    cout << sum << endl;
+    return 0;
+}
